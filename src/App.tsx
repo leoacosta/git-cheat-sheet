7d6ad8945821f69873a80d7456ca4546b9628b8b
leoacosta <lacosmedia@gmail.com>
2021-08-14 22:00:00 +1000

    feat: refactor layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import commands from './data.json';
 import logoGit from './logo-git.svg';
 import logoGitHub from './logo-github.svg';
+import iconSearch from './icon-search.svg';
 
 interface Commands {
   name: string;
@@ -11,7 +12,7 @@ interface Commands {
 
 function App() {
   const [searchTerm, setSearchTerm] = useState('');
-  const [searchResults, setSearchResults] = useState<Commands[]>([]);
+  const [searchResults, setSearchResults] = useState<Commands[]>(commands);
 
   // filter items based on search term
   const results = commands.filter((command) =>
@@ -30,26 +31,13 @@ function App() {
 
   return (
     <div className="App">
-      <header className="header">
-        <div className="d-flex">
-          <img src={logoGit} alt="Git Logo" className="logo" />
+      <header className="site-header">
+        <div className="site-branding">
+          <img className="site-logo" src={logoGit} alt="Git Logo" />
           <p className="site-title">The Git cheat sheet</p>
         </div>
-        <a
-          className="d-flex"
-          href="https://github.com/leoacosta/git-cheat-sheet/"
-        >
-          <img src={logoGitHub} alt="GitHub Logo" className="logo--small" />
-          <p className="site-contribution">View on GitHub</p>
-        </a>
-      </header>
-      <main className="App-header">
-        <p>
-          <code>Search for any git command you can think of.</code>
-        </p>
-        <div className="search">
+        <div className="site-search">
           <input
-            className="search-input"
             type="text"
             placeholder="Search git command"
             value={searchTerm}
@@ -60,17 +48,16 @@ function App() {
             className="search-button"
             onClick={() => setSearchResults(results)}
           >
-            <svg
-              width="32"
-              height="32"
-              version="1.1"
-              viewBox="0 0 32 32"
-              aria-hidden="false"
-            >
-              <path d="M22 20c1.2-1.6 2-3.7 2-6 0-5.5-4.5-10-10-10S4 8.5 4 14s4.5 10 10 10c2.3 0 4.3-.7 6-2l6.1 6 1.9-2-6-6zm-8 1.3c-4 0-7.3-3.3-7.3-7.3S10 6.7 14 6.7s7.3 3.3 7.3 7.3-3.3 7.3-7.3 7.3z"></path>
-            </svg>
+            <img src={iconSearch} alt="Search" style={{ width: 30 }} />
           </button>
         </div>
+        <div className="site-contribution">
+          <a href="https://github.com/leoacosta/git-cheat-sheet/">
+            <img src={logoGitHub} alt="Contribute on GitHub" />
+          </a>
+        </div>
+      </header>
+      <main className="site-content">
         {searchResults.map((item: Commands, index) => (
           <div className="search-result" key={index}>
             <h2>
